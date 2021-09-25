2aa2b2ff93bba4321ab3a860e7ad35d0b66e0501
Andile Jaden Mbele <andilembele020@gmail.com>
2021-09-25 08:53:19 +0200

    Done

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 const app = Vue.createApp({
-  template: "<h1>Hello World</h1>",
+  data() {
+    return {
+      firstName: "Andile",
+      lastName: "Mbele",
+      email: "nathaniel@gmail.com",
+      gender: "male",
+      picture: "https://randomuser.me/api/portraits/men/45.jpg",
+    };
+  },
+  methods: {
+    async getUser() {
+      const res = await fetch("https://randomuser.me/api");
+      const { results } = await res.json();
+      console.log(results);
+
+      this.firstName = results[0].name.first;
+      this.lastName = results[0].name.last;
+      this.email = results[0].email;
+      this.gender = results[0].gender;
+      this.picture = results[0].picture.large;
+    },
+  },
 });
 
 app.mount("#app");
